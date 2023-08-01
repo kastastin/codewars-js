879f6a3b77f3ed22f0bb9570b71d9a3493513d95
kastastin <kostya242421@gmail.com>
2023-08-01 03:11:16 +0300

    Unique Sum

---
+++ Fundamentals/Arrays/uniqueSum.js
@@ -0,0 +1,24 @@
+// <-- Unique Sum -->
+
+/*
+  Given a list of integers values, your job is to return the sum of the values; however, if the same integer value appears multiple times in the list, you can only count it once in your sum.
+
+  For example:
+  [ 1, 2, 3] ==> 6
+  [ 1, 3, 8, 1, 8] ==> 12
+  [ -1, -1, 5, 2, -7] ==> -1
+  [] ==> null
+*/
+
+// <-- My Solution -->
+function uniqueSum(lst) {
+  if (lst.length === 0) return null;
+  return [...new Set(lst)].reduce((acc, cur) => acc + cur, null);
+}
+
+// <-- Best Solution -->
+function uniqueSumBest(lst) {
+  return lst.length
+    ? [...new Set(lst)].reduce((acc, cur) => acc + cur, 0)
+    : null;
+}
