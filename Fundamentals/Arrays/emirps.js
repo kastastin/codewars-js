1e95faaa246a5f474ae0ce46cf45a8fcf7975e07
kastastin <kastastin.main@gmail.com>
2024-10-09 00:42:34 +0300

    Emirps

---
+++ Fundamentals/Arrays/emirps.js
@@ -0,0 +1,82 @@
+// <-- Emirps -->
+
+/*
+  If you reverse the word "emirp" you will have the word "prime". That idea is related with the purpose of this kata: we should select all the primes that when reversed are a different prime (so palindromic primes should be discarded).
+
+  For example: 13, 17 are prime numbers and the reversed respectively are 31, 71 which are also primes, so 13 and 17 are "emirps". But primes 757, 787, 797 are palindromic primes, meaning that the reversed number is the same as the original, so they are not considered as "emirps" and should be discarded.
+
+  Your task
+  Create a function that receives one argument n, as an upper limit, and the return the following array:
+
+  [number_of_emirps_below_n, largest_emirp_below_n, sum_of_emirps_below_n]
+
+  Examples
+  find_emirp(10)
+  [0, 0, 0] ''' no emirps below 10 '''
+
+  find_emirp(50)
+  [4, 37, 98] ''' there are 4 emirps below 50: 13, 17, 31, 37; largest = 37; sum = 98 '''
+
+  find_emirp(100)
+  [8, 97, 418] ''' there are 8 emirps below 100: 13, 17, 31, 37, 71, 73, 79, 97; largest = 97; sum = 418 '''
+
+*/
+
+// <-- My Solution -->
+function findEmirp(n) {
+  const isPrime = (num) => {
+    for (let i = 2, s = Math.sqrt(num); i <= s; i++) {
+      if (num % i === 0) return false;
+    }
+
+    return num > 1;
+  };
+
+  let emirp = [];
+
+  for (let i = 2; i < n; i++) {
+    reverseI = i.toString().split("").reverse().join("");
+
+    if (isPrime(i) && reverseI !== i.toString() && isPrime(parseInt(reverseI)))
+      emirp.push(i);
+  }
+
+  return [
+    emirp.length,
+    Math.max(...emirp),
+    emirp.reduce((sum, a) => sum + a, 0),
+  ];
+}
+
+// <-- Best Solution -->
+function findEmirpBest(n) {
+  const isPrime =(num) => {
+    if (num % 2 == 0) return false;
+
+    for (var i = 3; i <= Math.sqrt(num); i += 2) {
+      if (num % i == 0) return false;
+    }
+
+    return true;
+  }
+  
+  var ans = [],
+    st = 13,
+    rev,
+    sm = 0,
+    cnt = 0;
+  
+  for (var i = st; i <= n; i += 2) {
+    if (isPrime(i)) {
+      rev = +i.toString().split("").reverse().join("");
+
+      if (rev !== i && isPrime(rev)) {
+        ans.push(i);
+        sm += i;
+        cnt++;
+      }
+    }
+  }
+  
+  return [cnt, ans[cnt - 1], sm];
+}
