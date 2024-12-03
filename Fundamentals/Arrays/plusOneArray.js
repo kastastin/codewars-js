e426693dfd07bd5e97a2e0183c254fc7af894a32
kastastin <kastastin.main@gmail.com>
2024-12-03 02:23:24 +0200

    +1 Array

---
+++ Fundamentals/Arrays/plusOneArray.js
@@ -0,0 +1,53 @@
+// <-- +1 Array -->
+
+/*
+  Given an array of integers of any length, return an array that has 1 added to the value represented by the array.
+
+  If the array is invalid (empty, or contains negative integers or integers with more than 1 digit), return null.
+
+  Examples
+  Valid arrays
+
+  [4, 3, 2, 5] would return [4, 3, 2, 6] (4325 + 1 = 4326)
+  [1, 2, 3, 9] would return [1, 2, 4, 0] (1239 + 1 = 1240)
+  [9, 9, 9, 9] would return [1, 0, 0, 0, 0] (9999 + 1 = 10000)
+  [0, 1, 3, 7] would return [0, 1, 3, 8] (0137 + 1 = 0138)
+  Invalid arrays
+
+  [] is invalid because it is empty
+  [1, -9] is invalid because -9 is not a non-negative integer
+  [1, 2, 33] is invalid because 33 is not a single-digit integer
+*/
+
+// <-- My Solution -->
+function upArray(arr) {
+  if (!arr.length || arr.some((el) => el < 0 || el > 9)) return null;
+
+  const res = (Number(arr.join("")) + 1)
+    .toString()
+    .split("")
+    .map((el) => +el);
+
+  if (res.length === 1) {
+    res.unshift(0);
+  }
+
+  return res;
+}
+
+console.log(upArray([4, 3, 2, 5]));
+
+// <-- Best Solution -->
+function upArrayBest(arr) {
+  if (!arr.length || arr.some((el) => el < 0 || el > 9)) return null;
+
+  let i = arr.length - 1;
+
+  while (i >= 0 && arr[i] == 9) {
+    arr[i--] = 0;
+  }
+
+  i < 0 ? arr.unshift(1) : arr[i]++;
+
+  return arr;
+}
