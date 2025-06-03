53297feee38a1309890357ea9a2e1e65547d3386
kastastin <kastastin.main@gmail.com>
2025-06-03 10:57:21 +0300

    Simple Sentences

---
+++ Fundamentals/Strings/simpleSentences.js
@@ -0,0 +1,31 @@
+// <-- Simple Sentences -->
+
+/*
+  Implement a function, so it will produce a sentence out of the given parts.
+
+  Array of parts could contain:
+
+  words;
+  commas in the middle;
+  multiple periods at the end.
+  Sentence making rules:
+
+  there must always be a space between words;
+  there must not be a space between a comma and word on the left;
+  there must always be one and only one period at the end of a sentence.
+  Example:
+
+  makeSentence(['hello', ',', 'my', 'dear']) // returns 'hello, my dear.'
+*/
+
+// <-- My Solution -->
+function makeSentence(parts) {
+  return (parts.join(" ") + ".")
+    .replace(/ \./g, ".")
+    .replace(/ ,/g, ",")
+    .replace(/\.+$/, ".");
+}
+
+// <-- Best Solution -->
+const makeSentenceBest = (c) =>
+  c.join(" ").replace(/(?<=\w) (?=\W)|[.\s]+$/g, "") + ".";
