e5cea2a7da522e6da9d4ffcbd5d5ca35965fabdf
kastastin <kastastin.main@gmail.com>
2025-12-05 00:11:19 +0200

    Perfect Square

---
+++ Fundamentals/Strings/perfectSquare.js
@@ -0,0 +1,42 @@
+// <-- Perfect Square -->
+
+/*
+  Write function which validates an input string. If the string is a perfect square return true,false otherwise.
+
+  What is perfect square?
+  * We assume that character '.' (dot) is a perfect square (1x1) * Perfect squares can only contain '.' (dot) and optionally '\n' (line feed) characters.
+  * Perfect squares must have same width and height -> cpt.Obvious
+  * Squares of random sizes will be tested!
+  Function input:
+  perfectSquare = "...\n...\n...";    
+
+  This represents the following Perfect Square:
+
+  `...
+  ...
+  ...`
+                                
+  notPerfect = "..,\n..\n...";
+
+  This is not a Perfect Square:
+
+  `..,
+  ..
+  ...`
+*/
+
+// <-- My Solution -->
+function perfectSquare(s) {
+  if (/[^.\n]/.test(s)) {
+    return false;
+  }
+
+  s = s.split("\n");
+
+  return s.every((row) => row.length == s.length);
+}
+
+// <-- Best Solution -->
+function perfectSquareBest(string) {
+  return string.split(/\n/).every((val, _, arr) => val === `.`.repeat(arr.length));
+}
