3ca05c02bb1c244662cdad266a808c25a630129e
kastastin <kastastin.main@gmail.com>
2026-03-18 11:01:54 +0200

    Permute a Palindrome

---
+++ Fundamentals/Strings/permutePalindrome.js
@@ -0,0 +1,26 @@
+// <-- Permute a Palindrome -->
+
+/*
+  Write a function that will check whether ANY permutation of the characters of the input string is a palindrome. Bonus points for a solution that is efficient and/or that uses only built-in language functions. Deem yourself brilliant if you can come up with a version that does not use any function whatsoever.
+
+  For this kata assume that all characters are lowercase.
+
+  Example
+  madam -> True
+  adamm -> True
+  junk -> False
+
+  Hint
+  The brute force approach would be to generate all the permutations of the string and check each one of them whether it is a palindrome. However, an optimized approach will not require this at all.
+*/
+
+// <-- Solution -->
+function permuteAPalindrome(str) {
+  const set = new Set();
+
+  for (const char of str) {
+    set[set.has(char) ? "delete" : "add"](char);
+  }
+
+  return set.size < 2;
+}
