eb8332660f4d516257e935ecec463d0cd3c1579e
kastastin <kastastin.main@gmail.com>
2026-04-21 13:03:50 +0300

    Convert Hex to RGB

---
+++ Fundamentals/Strings/hexToRGB.js
@@ -0,0 +1,21 @@
+// <-- Convert Hex to RGB -->
+
+/*
+  When working with color values it can sometimes be useful to extract the individual red, green, and blue (RGB) component values for a color. Implement a function that meets these requirements:
+
+  Accepts a case-insensitive hexadecimal color string as its parameter (ex. "#FF9933" or "#ff9933")
+  Returns a Map<String, int> with the structure {r: 255, g: 153, b: 51} where r, g, and b range from 0 through 255
+  Note: your implementation does not need to support the shorthand form of hexadecimal notation (ie "#FFF")
+
+  Example
+  "#FF9933" --> {r: 255, g: 153, b: 51}
+*/
+
+// <-- Solution -->
+function hexStringToRGB(hex) {
+  return {
+    r: parseInt(hex.substr(1, 2), 16),
+    g: parseInt(hex.substr(3, 2), 16),
+    b: parseInt(hex.substr(5, 2), 16),
+  };
+}
