68053cd6e2a091e59bf5d153d4a3708614c46b8e
kastastin <kastastin.main@gmail.com>
2026-05-15 11:10:51 +0300

    Ratio of Bouncy Numbers

---
+++ Fundamentals/Numbers/ratioBouncyNumbers.js
@@ -0,0 +1,48 @@
+// <-- Ratio of Bouncy Numbers -->
+
+/*
+  Working from left-to-right if no digit is exceeded by the digit to its left it is called an increasing number; for example, 134468.
+
+  Similarly if no digit is exceeded by the digit to its right it is called a decreasing number; for example, 66420.
+
+  We shall call a positive integer that is neither increasing nor decreasing a "bouncy" number; for example, 155349.
+
+  Clearly there cannot be any bouncy numbers below one-hundred, but just over half of the numbers below one-thousand (525) are bouncy. In fact, the least number for which the proportion of bouncy numbers first reaches 50% is 538.
+
+  Surprisingly, bouncy numbers become more and more common and by the time we reach 21780 the proportion of bouncy numbers is equal to 90%.
+
+  Your Task
+  Complete the bouncyRatio function.
+
+  The input will be the target ratio.
+
+  The output should be the smallest number such that the proportion of bouncy numbers reaches the target ratio.
+
+  You should throw an Error for a ratio less than 0% or greater than 99%.
+*/
+
+// <-- Solution -->
+function isBouncy(num) {
+  if (num < 101) {
+    return false;
+  }
+
+  const digits = String(num).split("");
+  const incr = +digits.sort().join("");
+  const decr = +digits.reverse().join("");
+
+  return incr !== decr && num !== incr && num !== decr;
+}
+
+function bouncyRatio(ratio) {
+  let number = 1;
+  let count = 0;
+
+  while (count / number < ratio) {
+    if (isBouncy(++number)) {
+      count++;
+    }
+  }
+
+  return number;
+}
