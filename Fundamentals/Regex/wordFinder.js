cc96d4e6f51c5d11125ee4df016ddc5890757c3c
kastastin <kastastin.main@gmail.com>
2026-06-18 10:43:57 +0300

    Word Finder

---
+++ Fundamentals/Regex/wordFinder.js
@@ -0,0 +1,30 @@
+// <-- Word Finder -->
+
+/*
+  In this kata you have to extend the dictionary with a method, that returns a list of words matching a pattern. This pattern may contain letters (lowercase) and placeholders ("?"). A placeholder stands for exactly one arbitrary letter.
+
+  Example:
+
+  var fruits = new Dictionary(['banana', 'apple', 'papaya', 'cherry']);
+  fruits.getMatchingWords('lemon');     // must return []
+  fruits.getMatchingWords('cherr??');   // must return []
+  fruits.getMatchingWords('?a?a?a');    // must return ['banana', 'papaya']
+  fruits.getMatchingWords('??????');    // must return ['banana', 'papaya', 'cherry']
+  Additional Notes:
+
+  the words and patterns are all lowercase
+  the order of the returned words doesn't matter
+*/
+
+// <-- Solution -->
+class Dictionary {
+  constructor(words) {
+    this.words = words;
+  }
+
+  getMatchingWords(pattern) {
+    let reg = new RegExp("^" + pattern.replace(/\?/g, ".") + "$");
+
+    return this.words.filter(reg.test, reg);
+  }
+}
