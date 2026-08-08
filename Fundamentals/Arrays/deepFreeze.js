9db4cdba403c615c2106a06264e0c5fd734518ad
kastastin <kastastin.main@gmail.com>
2026-08-08 08:30:10 +0300

    Deep Freeze

---
+++ Fundamentals/Arrays/deepFreeze.js
@@ -0,0 +1,22 @@
+// <-- Deep Freeze -->
+
+/*
+  Create a complement to the Object.freeze function, Object.deepFreeze
+
+  This method should apply the Object.freeze function to an object and, recursively, all of its properties that are objects.
+
+  The freeze operation should prevent objects from being modified.
+*/
+
+// <-- Solution -->
+Object.deepFreeze = function (object) {
+  if (typeof object != "object") {
+    return;
+  }
+
+  for (const i in object) {
+    Object.deepFreeze(object[i]);
+  }
+
+  return Object.freeze(object);
+};
