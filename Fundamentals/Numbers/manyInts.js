720085832c265da7b85d08db94adb9bba62ee699
kastastin <kastastin.main@gmail.com>
2026-09-13 23:47:37 +0300

    Sum of many ints

---
+++ Fundamentals/Numbers/manyInts.js
@@ -0,0 +1,16 @@
+// <-- Sum of many ints -->
+
+/*
+  Write function
+
+  for i from 1 to n, do i % m and return the sum
+
+  f(n=10, m=5) // returns 20 (1+2+3+4+0 + 1+2+3+4+0)
+
+  You'll need to get a little clever with performance, since n can be a very large number
+*/
+
+// <-- Solution -->
+function f(n, m) {
+  return (Math.floor(n / m) * m * (m - 1)) / 2 + ((n % m) * ((n % m) + 1)) / 2;
+}
